0be436267e5f247bedff3544a1b3642c8d735887
ist0k <ist0k@users.noreply.github.com>
2016-12-17 02:45:13 +1300

    chanVersion.tcl
    chanver.tcl - Public Command Version TCL (sending output to specific channel).

--- chanver.tcl
+++ chanver.tcl
@@ -1,4 +1,4 @@
-# $Id: channelVersion.tcl,v1.OH 17/12/2016 02:25:40pm GMT +13 (NZ-DST) istok Exp $
+# $Id: channelVersion.tcl,v1.OH 17/12/2016 02:25:40am GMT +13 (NZ-DST) istok Exp $
 
 # Set the global trigger you wish to use (default: !)
 set chanVerTrig "!"
